59a4bc5701c3bfd93a4bc2cec278f19f920c01d6
edenb <eribog@msn.com>
2019-08-18 14:19:32 +0200

    Make logger configurable

--- src/logger.js
+++ src/logger.js
@@ -1,22 +1,39 @@
 'use strict';
+const config = require('config');
 const { createLogger, format, transports } = require('winston');
 const path = require('path');
 
-const logger = createLogger({
-    level: 'info',
-    format: format.combine(
-        format.label({ label: path.basename(process.mainModule.filename) }),
-        format.colorize(),
-        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        format.printf(
-            info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
+const consoleLogFormat = format.combine(
+    format.colorize(),
+    format.label({ label: path.basename(process.mainModule.filename) }),
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf(
+        info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
     )
-  ),
-  transports: [new transports.Console()]
+);
+
+const consoleLogFormatNoColor = format.combine(
+    format.label({ label: path.basename(process.mainModule.filename) }),
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf(
+        info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
+    )
+);
+
+const logger = createLogger({
+    transports: [
+        new transports.Console({
+            format: config.get('logger.colorize')?consoleLogFormat:consoleLogFormatNoColor,
+            level: config.get('logger.level'),
+            handleExceptions: true,
+            json: false
+        })
+    ]
 });
 
 logger.stream = {
-    write: function(message, encoding) {
+    write: function(message) {
+        // Trim message to remove empty line
         logger.info(message.trim());
     }
 };
