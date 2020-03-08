1002942ee120dd40e830ea235a94467189049c94
edenb <eribog@msn.com>
2020-03-08 18:59:31 +0100

    Enable CORS for API

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,19 @@ const staticLayers = require('../controllers/staticlayer');
 module.exports = (passport) => {
     const router = express.Router();
 
+    // Enable CORS for API
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        // intercept OPTIONS method
+        if (req.method === 'OPTIONS') {
+            res.sendStatus(200);
+        } else {
+            next();
+        }
+    });
+
     router.get('/', (req, res) => {
         res.status(200).send(`API V1 is up`);
     });
