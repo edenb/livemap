783070e4535592669189c7f475e4b992e6163a30
edenb <eribog@msn.com>
2020-12-15 20:06:22 +0100

    Fix failing test

--- test/webhook.test.js
+++ test/webhook.test.js
@@ -5,11 +5,8 @@ const express = require('express');
 const usr = require('../models/user');
 const dev = require('../models/device');
 const webhook = require('../services/webhook');
-const livesvr = require('../services/liveserver');
 
 chai.use(chaihttp);
-
-livesvr.start();
 
 // Setup test user
 let testUser = {
