9ce28b1b3f0c4f39febfd5bbba42b41663db3052
edenb <eribog@msn.com>
2021-05-05 13:47:30 +0200

    Make mqtt info conditional

--- controllers/server.js
+++ controllers/server.js
@@ -3,22 +3,32 @@ const config = require('config');
 const mqtt = require('../services/mqtt');
 
 exports.getInfo = (req, res) => {
-    const info = { application: getApplicationInfo(), mqtt: getMqttInfo() };
+    const info = { ...getApplicationInfo(), ...getMqttInfo() };
     res.status(200).send(info);
 };
 
 function getApplicationInfo() {
-    return {
-        name: config.get('wclient.name'),
-        about: config.get('wclient.about'),
-        license: config.get('wclient.license'),
+    const applicationInfo = {
+        application: {
+            name: config.get('wclient.name'),
+            about: config.get('wclient.about'),
+            license: config.get('wclient.license'),
+        },
     };
+    return applicationInfo;
 }
 
 function getMqttInfo() {
-    const broker = mqtt.getBrokerUrl();
-    return {
-        url: `${broker.protocol}//${broker.hostname}`,
-        port: `${broker.port}`,
-    };
+    if (config.get('wclient.showBroker') === 'true') {
+        const broker = mqtt.getBrokerUrl();
+        const mqttInfo = {
+            mqtt: {
+                url: `${broker.protocol}//${broker.hostname}`,
+                port: `${broker.port}`,
+            },
+        };
+        return mqttInfo;
+    } else {
+        return {};
+    }
 }
