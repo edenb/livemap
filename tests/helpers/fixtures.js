8e0b08cca219f0df576de196cc496e55c3b1453b
edenb <eribog@msn.com>
2024-06-01 20:19:07 +0200

    Add tests for rest api

--- tests/helpers/fixtures.js
+++ tests/helpers/fixtures.js
@@ -16,6 +16,11 @@ export const adm1Devs = [
         identifier: 'adm1Dev1',
         alias: 'Admin 1 device 1',
     },
+    {
+        api_key: 'apikey-adm1',
+        identifier: 'adm1Dev2',
+        alias: 'Admin 1 device 2',
+    },
 ];
 
 export const man1Auth = {
@@ -35,6 +40,11 @@ export const man1Devs = [
         api_key: 'apikey-man1',
         identifier: 'man1Dev1',
         alias: 'Manager 1 device 1',
+    },
+    {
+        api_key: 'apikey-man1',
+        identifier: 'man1Dev2',
+        alias: 'Manager 1 device 2',
     },
 ];
 
@@ -56,6 +66,11 @@ export const vwr1Devs = [
         identifier: 'vwr1Dev1',
         alias: 'Viewer 1 device 1',
     },
+    {
+        api_key: 'apikey-vwr1',
+        identifier: 'vwr1Dev2',
+        alias: 'Viewer 1 device 2',
+    },
 ];
 
 export const vwr2Auth = {
@@ -75,5 +90,37 @@ export const vwr2Devs = [
         api_key: 'apikey-vwr2',
         identifier: 'vwr2Dev1',
         alias: 'Viewer 2 device 1',
+    },
+];
+
+export const vwr3Auth = {
+    password: 'password-vwr3',
+};
+
+export const vwr3 = {
+    username: 'vwr3',
+    fullname: 'Viewer 3',
+    email: 'viewer3@example.com',
+    role: 'viewer',
+    api_key: 'apikey-vwr3',
+};
+
+export const vwr3Devs = [
+    {
+        api_key: 'apikey-vwr3',
+        identifier: 'vwr3Dev1',
+        alias: 'Viewer 3 device 1',
+    },
+];
+
+export const devPositions = [
+    {
+        device_id: null,
+        device_id_tag: null,
+        loc_timestamp: '2019-01-01T00:00:00.000Z',
+        loc_lat: 40.1,
+        loc_lon: -73.1,
+        loc_type: null,
+        loc_attr: null,
     },
 ];
