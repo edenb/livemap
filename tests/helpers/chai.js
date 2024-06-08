30e97702ac789cced7a3cbb6af3feadb68ca87f6
edenb <eribog@msn.com>
2024-06-08 14:24:40 +0200

    Upgrade chai-http

--- tests/helpers/chai.js
+++ tests/helpers/chai.js
@@ -1,12 +1,8 @@
-// This helper is a workaround for an issue with Chai and the chai-http plugin.
-// At the moment Chai plugins can only be registered ones with the use() function.
-// This helper instantiates Chai with chai-http extension and provides a requester
-// for all tests to use.
-// Issue: https://github.com/chaijs/chai/issues/1603
 import { use } from 'chai';
 import chaiHttp from 'chai-http';
+const chai = use(chaiHttp);
 
-export const { request } = use(chaiHttp);
+export const request = chai.request.execute;
 
 // Create a subset of an object array that only contains the given keys
 // data = [ { a: 1, b: 2, c: 3 }, { a: 4, b: 5, c: 6 } ];
