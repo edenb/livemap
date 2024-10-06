16f21e6f013d6c74bcfa64e386b7071e63690a63
edenb <eribog@msn.com>
2024-10-06 14:25:24 +0200

    Remove chai-http workaround

--- tests/helpers/chai.js
+++ tests/helpers/chai.js
@@ -1,14 +1,10 @@
-// This helper is a workaround for an issue with Chai and the chai-http plugin.
-// At the moment Chai plugins can only be registered ones with the use() function.
-// This helper instantiates Chai with chai-http extension and provides a requester
-// for all tests to use.
-// Issue: https://github.com/chaijs/chai/issues/1603
-import { use } from 'chai';
-import chaiHttp from 'chai-http';
-const chai = use(chaiHttp);
+import * as chai from 'chai';
+import { default as chaiHttp, request as chaiRequest } from 'chai-http';
 
-export const request = chai.request.execute;
-export const agent = chai.request.agent;
+chai.use(chaiHttp);
+
+export const request = chaiRequest.execute;
+export const agent = chaiRequest.agent;
 
 // Create a subset of an object array that only contains the given keys
 // data = [ { a: 1, b: 2, c: 3 }, { a: 4, b: 5, c: 6 } ];
